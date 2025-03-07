c1f8ebc295649115209e7febd3a6b34eee54c63e
VincentRolongDevelop <vincent.rolong.develop@gmail.com>
2025-03-07 18:46:02 -0500

    Cambio de nombre

--- lib/services/api_session_client_services/Http.dart
+++ lib/services/api_session_client_services/Http.dart
@@ -8,9 +8,9 @@ class Http {
   final String baseUrl = kIsWeb
         // ? "http://3.129.92.139" // URL WEB
      // : "http://3.129.92.139"; // URL ANDROID
-      ? "http://3.144.104.212" // URL WEB
+      ? "http://localhost:5000" // URL WEB
       //? "" // URL WEB
-      : "http://3.144.104.212"; // URL ANDROID
+      : "http://10.0.2.2:5000"; // URL ANDROID
 
   Future<http.Response> login(String path, dynamic data) async {
     final url = Uri.parse('$baseUrl$path');
